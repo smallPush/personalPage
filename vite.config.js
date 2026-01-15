2cd40410c826f369e72d4d05af6b840cfb64a01c
Rubén <r.pineda@ixiam.com>
2026-01-15 12:52:38 +0100

    Use root for custom domain

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/personalPage/', // Explicitly set repository name as base path
+  base: '/', // Use root for custom domain
 })
