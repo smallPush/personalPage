8bb84dc7f5ff7c1f2e71d59861fe80251b505ce2
Rubén <r.pineda@ixiam.com>
2026-02-18 10:42:58 +0100

    Add SEO, add new article

--- src/data/notices.js
+++ src/data/notices.js
@@ -1,5 +1,10 @@
 export const notices = [
     {
+        id: 'civicrm-tuning',
+        filename: 'civicrm_tuning.md',
+        date: '2026-02-18'
+    },
+    {
         id: 'welcome',
         filename: 'welcome.md',
         date: '2026-01-15'
