3b6a7db5996d1daeaca530fdada76c0955aa5bda
smallPush <ruben.vilassar@gmail.com>
2026-05-23 15:58:25 +0000

    Deploying to gh-pages from @ smallPush/personalPage@676b219869c278fb146458d498586cab1882c772 🚀

---
+++ assets/channel-Bv8R0sIT.js
@@ -0,0 +1,1 @@
+import{ai as o,aj as n}from"./index-DI9MH39m.js";const t=(a,r)=>o.lang.round(n.parse(a)[r]);export{t as c};
--- assets/channel-CSOEtzcN.js
+++
@@ -1,1 +0,0 @@
-import{ai as o,aj as n}from"./index-CoOey97a.js";const t=(a,r)=>o.lang.round(n.parse(a)[r]);export{t as c};
